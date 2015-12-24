dbc89b3999f93ba32c05885d3bce86831b575477
C.T. Lin <chentsulin@gmail.com>
2015-12-24 14:17:13 +0800

    todos reducer Implement

---
+++ src/reducers/todos.js
@@ -0,0 +1,38 @@
+import { ADD_TODO, DELETE_TODO, TOGGLE_TODO } from '../actionTypes';
+
+const initialState = [
+  {
+    text: 'Use Redux',
+    isCompleted: false,
+    id: 0
+  }
+];
+
+export default function todos(state = initialState, action) {
+  switch (action.type) {
+  case ADD_TODO:
+    return [
+      {
+        id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
+        isCompleted: false,
+        text: action.text
+      },
+      ...state
+    ];
+
+  case DELETE_TODO:
+    return state.filter(todo =>
+      todo.id !== action.id
+    );
+
+  case TOGGLE_TODO:
+    return state.map(todo =>
+      todo.id === action.id ?
+        Object.assign({}, todo, { isCompleted: !todo.isCompleted }) :
+        todo
+    );
+
+  default:
+    return state;
+  }
+}
